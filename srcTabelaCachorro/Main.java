b59fb3a1d19bc5606d8a8ec8d92ac6813e32b469
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2025-03-11 20:48:48 -0300

    Exercicio
    com o intuito de ganhar pontos.

---
+++ srcTabelaCachorro/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Criando os Objetos auau
+        Cachorro caramelo = new Cachorro();
+        caramelo.nome = "Caramelo";
+        caramelo.raça = "Vira-lata";
+        caramelo.cor = "Amarelo";
+        caramelo.idade = 8;
+        caramelo.id = 1;
+        //Print para exibir os dados
+        caramelo.imprimirDados();
+
+        Cachorro xuxa = new Cachorro();
+        xuxa.nome = "Xuxa";
+        xuxa.raça = "Dálmata";
+        xuxa.cor = "Branca";
+        xuxa.idade = 3;
+        xuxa.id = 4;
+        xuxa.imprimirDados();
+
+        Cachorro lola = new Cachorro();
+        lola.nome = "Lola";
+        lola.raça = "Golden Retriever";
+        lola.cor = "Amarela";
+        lola.idade = 4;
+        lola.id = 3;
+        lola.imprimirDados();
+
+        Cachorro bolinha = new Cachorro();
+        bolinha.nome = "Bolinha";
+        bolinha.raça = "Pinscher";
+        bolinha.cor = "Preto";
+        bolinha.idade = 12;
+        bolinha.id = 2;
+        bolinha.imprimirDados();
+
+
+        //André Oliveira 5° Semestre
+        //Chris Kuriak Chung Ferrari 5° Semestre
+        //Felipe Basilio romeu 4° Semestre
+        //Felipe Marcelino 5° Semestre
+    }
+
+}
