ef56484ca499baafa01b4793b4a0113a08d7b7d9
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2025-03-11 20:46:58 -0300

    Entendimento de herança

---
+++ srcMain&Class/Carro.java
@@ -0,0 +1,31 @@
+// no arquivo Carro.java
+public class Carro
+{
+
+    // propriedades (atributos) - caracteristicas
+    int quantidadePortas;
+    String cor;
+    Modelo modelo;
+
+
+    //metodos - açoes
+    public void acelerar()
+    {
+        // codigo que fara o o carro acelerar
+
+    }
+
+    public void frear()
+    {
+        // codigo que fara o carro fraear
+    }
+
+}
+
+enum Modelo
+{
+    HATCH,
+    SEDAN,
+    SUV,
+    UTILITARIO
+}
---
+++ srcMain&Class/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Objeto baseado no carro
+        Carro carroA = new Carro();
+        carroA.quantidadePortas = 4;
+        carroA.cor = "Azul";
+        carroA.modelo = Modelo.HATCH;
+
+
+
+    }
+}
