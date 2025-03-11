f9384c20984c22015e7f8fbf4243bb89e4c23708
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2025-03-11 20:39:07 -0300

    Extends

---
+++ ClasseHerdada.java
@@ -0,0 +1,22 @@
+public class Cachorro {
+    String nome;
+    String raça;
+    String cor;
+    int idade;
+    int id;
+
+    void imprimirDados()
+    {
+        System.out.print("Id: ");
+        System.out.println(id);
+        System.out.print("nome: ");
+        System.out.println(nome);
+        System.out.print("idade: ");
+        System.out.println(idade);
+        System.out.print("raça: ");
+        System.out.println(raça);
+        System.out.print("Cor: ");
+        System.out.println(cor);
+        System.out.println("------------");
+    }
+}
